e7178e4c9408924fcac51ba95221f766a4917e02
Ethan Ford <ethan.ford@duke.edu>
2018-12-08 16:21:48 -0500

    done

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -132,11 +132,6 @@ public class HuffProcessor {
 		HuffNode root = readTreeHeader(in); 
 		readCompressedBits(root,in,out);
 		out.close();
-		while(true) {
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
 	}
 	private HuffNode readTreeHeader(BitInputStream in) {
 		int bits = in.readBits(BITS_PER_INT);
